7b9afe854fa82e0f8e1f5703db5b0fe9e0481e3c
glaucoSapucaia <glaucopco@gmail.com>
2024-06-19 13:03:59 -0300

    Objeto TarefaService

---
+++ assets/tarefa_service.php
@@ -0,0 +1,15 @@
+<!-- Script para ESTUDOS! -->
+<!-- Num projeto real, arqquivos como este devem estar protegidos em diretório PRIVADOS -->
+
+<?php
+    // Classe TarefaService | CRUD
+    class TarefaService {
+        public function inserir() {}
+
+        public function recuperar() {}
+
+        public function atualizar() {}
+
+        public function remover() {}
+    }
+?>
